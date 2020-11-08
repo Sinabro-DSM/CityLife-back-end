3ab66d2c70e95f08b7e5f63c95a9e7d89c202d27
AHYEON04 <ahyeon3590@gmail.com>
2020-11-08 16:53:43 +0900

    user controller

--- controller/user/controller.js
+++ controller/user/controller.js
@@ -1,4 +1,5 @@
 const { User } = require("../../models");
+const jwt = require("jsonwebtoken");
 
 const signup = async (req, res) => {
   // const userId = req.body.userId;
@@ -35,8 +36,56 @@ const signup = async (req, res) => {
 
 const login = async (req, res) => {
   const { userId, password } = req.body;
+  const secret = req.app.get("jwt-secret");
+  try {
+    const user = await User.findOne({
+      where: { userId },
+    });
+    if (user.password !== password) {
+      throw new Error();
+    }
+    const token = jwt.sign(
+      {
+        userId,
+      },
+      secret,
+      {
+        expiresIn: "24h",
+      }
+    );
+    res.status(200).json({
+      message: "성공",
+      accessToken: token,
+    });
+  } catch (err) {
+    console.log(err.message);
+    res.status(404).json({
+      message: "틀린 비밀번호",
+    });
+  }
+};
+const info = async (req, res) => {
+  const { character } = req.body;
+  const userId = req.decoded.userId;
+  try {
+    await User.update(
+      {
+        character,
+      },
+      { where: { userId } }
+    );
+    res.status(200).json({
+      message: "성공",
+    });
+  } catch (err) {
+    res.status(404).json({
+      message: "userId가 없음",
+    });
+  }
 };
 
 module.exports = {
   signup,
+  login,
+  info,
 };
