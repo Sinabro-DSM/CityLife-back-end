be300d161c8c3da363e210b9db0011376fd52b8b
yangseungbin <tanja1206@naver.com>
2020-11-11 22:03:50 +0900

    오타 수정

--- controller/lotto/controller.js
+++ controller/lotto/controller.js
@@ -1,28 +1,21 @@
-const { User, Food, Lotto } = require("../../models");
-const jwt = require("jsonwebtoken");
+const { User } = require("../../models");
 
 const lotto = async (req, res) => {
   const { lotto } = req.body;
-  const token = req.decoded.uesrId;
-  //const secret = req.app.get("jwt-secret");
+  const token = req.decoded.userId;
   try {
     const user = await User.findOne({
       where: { userId: token },
     });
-    //로또 가격 확인후 고치기
     if (user.money < 300) {
       throw new Error();
     }
-    await User.increment(
-      { money: lotto },
-      {
-        where: { userId: token },
-      }
-    );
+    await user.increment({ money: lotto - 300 });
     res.status(200).json({
       message: "성공",
     });
   } catch (err) {
+    console.log(err.message);
     res.status(409).json({
       message: "잔액이 부족합니다.",
     });
